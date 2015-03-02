629f87d038edea4d2541736d83c5ce5fd719576a
maira786 <maira_ahmad@csu.fullerton.edu>
2015-03-02 00:16:55 -0800

    added functionality for screenshots to only show on the demo page

--- Assignment3/Amazeriffic/javascripts/app.js
+++ Assignment3/Amazeriffic/javascripts/app.js
@@ -27,12 +27,14 @@ var main = function () {
                 for (i = toDos.length-1; i >= 0; i--) {
                     $content.append($("<li>").text(toDos[i]));
                 }
+                $(".demo").hide();
             } else if ($element.parent().is(":nth-child(2)")) {
                 // oldest first, so we go through the array forwards
                 $content = $("<ul>");
                 toDos.forEach(function (todo) {
                     $content.append($("<li>").text(todo));
                 });
+                $(".demo").hide();
             } else if ($element.parent().is(":nth-child(3)")) {
                 // input a new to-do
                 $input = $("<input>"),
@@ -48,29 +50,10 @@ var main = function () {
                 $content = $("<div>").append($input).append($button);
                /* Alternatively append() allows multiple arguments so the above
                 can be done with $content = $("<div>").append($input, $button); */
+                $(".demo").hide();
             }
             else{
-         
-
-                //Examples of how to assign the Colorbox event to elements
-                $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
-                $(".callbacks").colorbox({
-                  onOpen:function(){ alert('onOpen: colorbox is about to open'); },
-                  onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
-                  onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
-                  onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-                  onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
-                });
-
-                $('.non-retina').colorbox({rel:'group5', transition:'none'})
-                $('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
-                
-                //Example of preserving a JavaScript event for inline calls.
-                $("#click").click(function(){ 
-                  $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-                  return false;
-                });
-      
+                $(".demo").show();
             }
 
             $("main .content").append($content);
