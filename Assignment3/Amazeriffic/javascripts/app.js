4456abec9e4f76f16acb4cb49729115b36a3efae
maira786 <maira_ahmad@csu.fullerton.edu>
2015-03-02 00:02:27 -0800

    added and implemented colorbox slideshow

--- Assignment3/Amazeriffic/javascripts/app.js
+++ Assignment3/Amazeriffic/javascripts/app.js
@@ -49,6 +49,29 @@ var main = function () {
                /* Alternatively append() allows multiple arguments so the above
                 can be done with $content = $("<div>").append($input, $button); */
             }
+            else{
+         
+
+                //Examples of how to assign the Colorbox event to elements
+                $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
+                $(".callbacks").colorbox({
+                  onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+                  onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+                  onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+                  onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+                  onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
+                });
+
+                $('.non-retina').colorbox({rel:'group5', transition:'none'})
+                $('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
+                
+                //Example of preserving a JavaScript event for inline calls.
+                $("#click").click(function(){ 
+                  $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+                  return false;
+                });
+      
+            }
 
             $("main .content").append($content);
 
