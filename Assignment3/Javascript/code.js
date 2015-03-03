b93d55075bb7beebfcd8b6753977b1364d7c2dcd
maira786 <maira_ahmad@csu.fullerton.edu>
2015-03-02 22:43:12 -0800

    added the last functionality for functions 4 and 5, also finished the styling

--- Assignment3/Javascript/code.js
+++ Assignment3/Javascript/code.js
@@ -56,11 +56,49 @@ var main = function () {
             }
         }
     };
-  
+    var frizzbuzz_4 = function ($outputwords) {
+        var $i, $output;
 
+        for($i=1; $i<=100; $i++){
+            if ($i%15 ===0){
+                $output = $("<p>").text($outputwords.divisibleByThree + $outputwords.divisibleByFive);
+                $(".output").append($output);
+            }else if ($i%3 ===0){
+                $output = $("<p>").text($outputwords.divisibleByThree);
+                $(".output").append($output);
+            }else if ($i%5 ===0){
+                $output = $("<p>").text($outputwords.divisibleByFive);
+                $(".output").append($output);
+            }else{
+                $output = $("<p>").text($i);
+                $(".output").append($output);
+            }
+        }
+    };
+    var frizzbuzz_5 = function ($arr, $outputwords) {
+        var $i, $output;
+
+        for($i=$arr[0]; $i<=$arr.length + 1; $i++){
+            if ($i%15 ===0){
+                $output = $("<p>").text($outputwords.divisibleByThree + $outputwords.divisibleByFive);
+                $(".output").append($output);
+            }else if ($i%3 ===0){
+                $output = $("<p>").text($outputwords.divisibleByThree);
+                $(".output").append($output);
+            }else if ($i%5 ===0){
+                $output = $("<p>").text($outputwords.divisibleByFive);
+                $(".output").append($output);
+            }else{
+                $output = $("<p>").text($i);
+                $(".output").append($output);
+            }
+        }
+    };
+    //----------------1------------------------
     $(".initiate_1 button").on("click", function (event) {
         frizzbuzz_1();
     });
+    //----------------2------------------------
     $(".initiate_2 button").on("click", function (event) {
         frizzbuzz_2($(".initiate_2 .start input").val(), $(".initiate_2 .end input").val());
     });
@@ -69,7 +107,7 @@ var main = function () {
             frizzbuzz_2($(".initiate_2 .start input").val(), $(".initiate_2 .end input").val());
         }
     });
-
+    //----------------3------------------------
     var $arr= [];
 
     //The + button action for entering array values
@@ -92,6 +130,39 @@ var main = function () {
         
         frizzbuzz_3($arr);
     }); 
+    //----------------4------------------------
+    var $outputwords = {divisibleByThree: "", divisibleByFive: ""};
+
+    $(".initiate_4  button").on("click", function (event) {
+        
+        frizzbuzz_4({ divisibleByThree: $(".initiate_4 .frizz input").val(), divisibleByFive: $(".initiate_4 .bizz input").val()});
+        $(".initiate_4 .frizz input").val("");
+        $(".initiate_4 .bizz input").val("");
+    }); 
+    //-----------------5-----------------------
+    //The + button action for entering array values
+    $(".initiate_5 .enter button").on("click", function (event) {
+        if ($(".initiate_5 .array-input input") !== "") {
+            $arr.push($(".initiate_5 .array-input input").val());
+            $(".initiate_5 .array-input input").val("");
+        }
+    });
+    //The enter keyboard key pressed to input array elements
+    $(".initiate_5 .array-input input").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            if ($(".initiate_5 .array-input input") !== "") {
+                $arr.push($(".initiate_5 .array-input input").val());
+                $(".initiate_5 .array-input input").val("");
+            }
+        }
+    });
+    $(".initiate_5 .calculate button").on("click", function (event) {
+        
+        $outputwords.divisibleByThree= $(".initiate_5 .frizz input").val();
+        $outputwords.divisibleByFive= $(".initiate_5 .bizz input").val();
+        frizzbuzz_5($arr, $outputwords);
+    }); 
+
 };
 
 $(document).ready(main);
