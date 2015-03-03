74afd0437ab97865b1c85746327f51e4a0af5569
maira786 <maira_ahmad@csu.fullerton.edu>
2015-03-02 18:43:34 -0800

    frizz bizz programs working up until 3, format for 4 in html and css is active

--- Assignment3/Javascript/code.js
+++ Assignment3/Javascript/code.js
@@ -6,17 +6,17 @@ var main = function () {
 
         for($i=1; $i<=100; $i++){
             if ($i%15 ===0){
-                $output = $("<p>").text("FizzBuzz");;
-                $(".output_1").append($output);
+                $output = $("<p>").text("FizzBuzz");
+                $(".output").append($output);
             }else if ($i%3 ===0){
-                $output = $("<p>").text("Fuzz");;
-                $(".output_1").append($output);
+                $output = $("<p>").text("Fuzz");
+                $(".output").append($output);
             }else if ($i%5 ===0){
-                $output = $("<p>").text("Buzz");;
-                $(".output_1").append($output);
+                $output = $("<p>").text("Buzz");
+                $(".output").append($output);
             }else{
-                $output = $("<p>").text($i);;
-                $(".output_1").append($output);
+                $output = $("<p>").text($i);
+                $(".output").append($output);
             }
         }
     };
@@ -24,20 +24,39 @@ var main = function () {
         var $i, $output;
         for($i=$start; $i<=$end; $i++){
             if ($i%15 ===0){
-                $output = $("<p>").text("FizzBuzz");;
+                $output = $("<p>").text("FizzBuzz");
                 $(".output").append($output);
             }else if ($i%3 ===0){
-                $output = $("<p>").text("Fuzz");;
+                $output = $("<p>").text("Fuzz");
                 $(".output").append($output);
             }else if ($i%5 ===0){
-                $output = $("<p>").text("Buzz");;
+                $output = $("<p>").text("Buzz");
                 $(".output").append($output);
             }else{
-                $output = $("<p>").text($i);;
+                $output = $("<p>").text($i);
                 $(".output").append($output);
             }
         }
     };
+    var frizzbuzz_3 = function ($arr) {
+        var $i, $output;
+        for($i=$arr[0]; $i<=$arr.length + 1; $i++){
+            if ($i%15 ===0){
+                $output = $("<p>").text("FizzBuzz");
+                $(".output").append($output);
+            }else if ($i%3 ===0){
+                $output = $("<p>").text("Fuzz");
+                $(".output").append($output);
+            }else if ($i%5 ===0){
+                $output = $("<p>").text("Buzz");
+                $(".output").append($output);
+            }else{
+                $output = $("<p>").text($i);
+                $(".output").append($output);
+            }
+        }
+    };
+  
 
     $(".initiate_1 button").on("click", function (event) {
         frizzbuzz_1();
@@ -50,7 +69,29 @@ var main = function () {
             frizzbuzz_2($(".initiate_2 .start input").val(), $(".initiate_2 .end input").val());
         }
     });
- 
+
+    var $arr= [];
+
+    //The + button action for entering array values
+    $(".initiate_3 .enter button").on("click", function (event) {
+        if ($(".initiate_3 input") !== "") {
+            $arr.push($(".initiate_3 input").val());
+            $(".initiate_3 input").val("");
+        }
+    });
+    //The enter keyboard key pressed to input array elements
+    $(".initiate_3 input ").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            if ($(".initiate_3 input") !== "") {
+                $arr.push($(".initiate_3 input").val());
+                $(".initiate_3 input").val("");
+            }
+        }
+    });
+    $(".initiate_3 .calculate button").on("click", function (event) {
+        
+        frizzbuzz_3($arr);
+    }); 
 };
 
 $(document).ready(main);
