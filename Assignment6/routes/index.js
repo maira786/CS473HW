b49789e654d16016f4e2586766fee6982dcca743
maira786 <maira_ahmad@csu.fullerton.edu>
2015-04-05 09:55:19 -0700

    set up initial project

--- Assignment6/routes/index.js
+++ Assignment6/routes/index.js
@@ -3,12 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hi from Maira' });
+  res.render('index', { title: 'Lets Play' });
+});
+router.get('/play/rock', function(req, res) {
+    res.render('index', { title: 'played rock' })
+});
+router.get('/play/paper', function(req, res) {
+    res.render('paper', { title: 'Hello, World!' })
+});
+router.get('/play/scissors', function(req, res) {
+    res.render('scissors', { title: 'Hello, World!' })
+});
+router.get('/play/lizard', function(req, res) {
+    res.render('lizard', { title: 'Hello, World!' })
+});
+router.get('/play/spock', function(req, res) {
+    res.render('spock', { title: 'Hello, World!' })
 });
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-    res.render('helloworld', { title: 'Hello, World!' })
-});
 
 module.exports = router;
