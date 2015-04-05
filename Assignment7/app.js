5e5265acb7b872ccc2e87b9678f36bdc9cdb6328
maira786 <maira_ahmad@csu.fullerton.edu>
2015-04-05 11:02:09 -0700

    general format of app running

--- Assignment7/app.js
+++ Assignment7/app.js
@@ -1,3 +1,7 @@
+/*
+run the app:
+     $ DEBUG=Assignment7:* ./bin/www
+*/
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,13 +10,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+////var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
+//app.engine('jade', require('jade').__express);
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -23,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+///app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
