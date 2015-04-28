7ddef4485791aecf484e5c41e94726a632902cd8
Maira Ahmad <maira_ahmad@csu.fullerton.edu>
2015-04-27 17:35:42 -0700

    before i make more changes

---
+++ Assignment9/server_2.js
@@ -0,0 +1,53 @@
+var app = require('express')();
+var http= require("http");
+var mongoose = require("mongoose");
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+io.on('connection', function(){ console.log('working'); });
+server.listen(3000);
+
+
+app.use(express.static(__dirname + "/client"));
+app.use(express.bodyParser());
+
+// connect to the amazeriffic data store in mongo
+mongoose.connect('mongodb://localhost/amazeriffic');
+
+// This is our mongoose model for todos
+var ToDoSchema = mongoose.Schema({
+    description: String,
+    tags: [ String ]
+});
+
+var ToDo = mongoose.model("ToDo", ToDoSchema);
+
+//http.createServer(app).listen(3000);
+
+app.get("/todos.json", function (req, res) {
+    ToDo.find({}, function (err, toDos) {
+	res.json(toDos);
+    });
+});
+
+app.post("/todos", function (req, res) {
+    console.log(req.body);
+    var newToDo = new ToDo({"description":req.body.description, "tags":req.body.tags});
+    newToDo.save(function (err, result) {
+	if (err !== null) {
+	    // the element did not get saved!
+	    console.log(err);
+	    res.send("ERROR");
+	} else {
+	    // our client expects *all* of the todo items to be returned, so we'll do
+	    // an additional request to maintain compatibility
+	    ToDo.find({}, function (err, result) {
+		if (err !== null) {
+		    // the element did not get saved!
+		    res.send("ERROR");
+		}
+		res.json(result);
+	    });
+	}
+    });
+});
+
